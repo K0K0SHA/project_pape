6e0e14d5bc7c24b397a67bcd7ba33e7d11055884
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-23 07:28:14 -0400

    set wallpaper
    needs gsettings

---
+++ change_bg_template.sh
@@ -0,0 +1,8 @@
+# sets a screen wallpaper using gsettings
+# TEMPLATE CODE, DO NOT RUN
+gsettings set  org.cinnamon.desktop.background picture-uri 'file:///home/pablo/Documents/project_pape/lewd/elastigirl.png'
+
+
+
+# REFERENCED FROM:
+# https://forums.linuxmint.com/viewtopic.php?t=177294
---
+++ pumpkin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# sets a screen wallpaper using gsettings
+# v 0.5 functioning prototype with hard-coded filepaths
+# written by K0K0$HA of GitHub
+
+# strengths
+# gsettings should come preinstalled with Mint/Ubuntu
+
+# weaknesses
+# requires absolute filepath
+
+gsettings set  org.cinnamon.desktop.background picture-uri 'file:///home/pablo/Pictures/wallpapers/halloween/pumpkin1.jpg'
+
+
+
+# REFERENCED FROM:
+# https://forums.linuxmint.com/viewtopic.php?t=177294
