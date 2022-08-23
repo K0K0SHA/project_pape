6c607646484621aa88ac4d01a261835bdbb7f323
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-23 07:31:13 -0400

    Update pumpkin.sh

--- pumpkin.sh
+++ pumpkin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# sets a screen wallpaper using gsettings
-# v 0.5 functioning prototype with hard-coded filepaths
+# sets a screen wallpaper using gsettings and org.cinnamon.desktop.background
+# Please edit filepath first! Code is in alpha.
+# v 0.5 functioning prototype with hard-coded filepaths, working on Mint
 # written by K0K0$HA of GitHub
 
 # strengths
-# gsettings should come preinstalled with Mint/Ubuntu
+# gsettings should come preinstalled with Mint
 
 # weaknesses
 # requires absolute filepath
